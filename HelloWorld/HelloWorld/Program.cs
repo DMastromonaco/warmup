fb006446c8a5c8e238e6f77136b58c79e3a3d8cd
DMastromonaco <derek.mastromonaco@gmail.com>
2015-07-29 10:49:03 -0700

    Hello world
    Initial commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿//A Hello World! proigram in C#
+using System;
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep console open
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
